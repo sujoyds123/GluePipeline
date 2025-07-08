1e4a0bac36e01587fca9c578b5aaa167c10bdb43
sujoyds123 <141669354+sujoyds123@users.noreply.github.com>
2025-07-08 22:05:13 +0530

    Create etl_script.py

---
+++ etl_script.py
@@ -0,0 +1,12 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+print("Hello from Glue ETL job!")
